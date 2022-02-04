b2cacdd8015bef2b199a3357906b7908b21cf3f5
IncogOwl <97695545+IncogOwl@users.noreply.github.com>
2022-02-04 15:18:45 -0800

    Update SkillDemoTest.java

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemoTest {
     @Test
     public void productTest() {
-        assertEquals(2, SkillDemo.product(2, 2)); // This test fails
+        assertEquals(2, SkillDemo.product(1, 2)); // This test fails
     }
 }
